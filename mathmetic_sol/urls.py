a1c9e52d4950f8324b66a438f9e1821b37267d8f
ErauQsPhan <mrsplol102@gmail.com>
2023-11-07 12:13:35 +0700

    upload

--- mathmetic_sol/urls.py
+++ mathmetic_sol/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('members.urls')),
 
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
